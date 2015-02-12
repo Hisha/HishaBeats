a6589b4b6c1c7cefc2a49d22f7003c96f6546b82
Kevin Smith <ksmith@hishatech.com>
2015-02-12 12:05:23 -0500

    - Upgraded to Version 0.0.3 - Main Activity button layout finished for now. - Started on preference id's and defaults in AppConstants.

---
+++ app/src/main/java/com/hishatech/android/hishabeats/AppConstants.java
@@ -0,0 +1,34 @@
+package com.hishatech.android.hishabeats;
+
+/**
+ * Created by smithkev on 2/12/2015.
+ */
+public class AppConstants {
+
+    public static final String pref_name = "HB_Prefs";
+    public static final String pref_btnC0R0 = "C0R0";
+    public static final String default_btnC0R0 = "";
+    public static final String pref_btnC0R1 = "C0R1";
+    public static final String default_btnC0R1 = "";
+    public static final String pref_btnC0R2 = "C0R2";
+    public static final String default_btnC0R2 = "";
+    public static final String pref_btnC0R3 = "C0R3";
+    public static final String default_btnC0R3 = "";
+    public static final String pref_btnC1R0 = "C1R0";
+    public static final String default_btnC1R0 = "";
+    public static final String pref_btnC1R1 = "C1R1";
+    public static final String default_btnC1R1 = "";
+    public static final String pref_btnC1R2 = "C1R2";
+    public static final String default_btnC1R2 = "";
+    public static final String pref_btnC1R3 = "C1R3";
+    public static final String default_btnC1R3 = "";
+    public static final String pref_btnC2R0 = "C2R0";
+    public static final String default_btnC2R0 = "";
+    public static final String pref_btnC2R1 = "C2R1";
+    public static final String default_btnC2R1 = "";
+    public static final String pref_btnC2R2 = "C2R2";
+    public static final String default_btnC2R2 = "";
+    public static final String pref_btnC2R3 = "C2R3";
+    public static final String default_btnC2R3 = "";
+
+}
--- app/src/main/java/com/hishatech/android/hishabeats/MainActivity.java
+++ app/src/main/java/com/hishatech/android/hishabeats/MainActivity.java
@@ -1,18 +1,43 @@
 package com.hishatech.android.hishabeats;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button btnC0R0, btnC0R1, btnC0R2, btnC0R3, btnC1R0, btnC1R1, btnC1R2, btnC1R3,
+            btnC2R0, btnC2R1, btnC2R2, btnC2R3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnC0R0 = (Button) findViewById(R.id.btnC0R0);
+        btnC0R1 = (Button) findViewById(R.id.btnC0R1);
+        btnC0R2 = (Button) findViewById(R.id.btnC0R2);
+        btnC0R3 = (Button) findViewById(R.id.btnC0R3);
+        btnC1R0 = (Button) findViewById(R.id.btnC1R0);
+        btnC1R1 = (Button) findViewById(R.id.btnC1R1);
+        btnC1R2 = (Button) findViewById(R.id.btnC1R2);
+        btnC1R3 = (Button) findViewById(R.id.btnC1R3);
+        btnC2R0 = (Button) findViewById(R.id.btnC2R0);
+        btnC2R1 = (Button) findViewById(R.id.btnC2R1);
+        btnC2R2 = (Button) findViewById(R.id.btnC2R2);
+        btnC2R3 = (Button) findViewById(R.id.btnC2R3);
+
+        SharedPreferences prefs = getSharedPreferences(AppConstants
+                .pref_name, 0);
+
+
+
+
     }
 
 
